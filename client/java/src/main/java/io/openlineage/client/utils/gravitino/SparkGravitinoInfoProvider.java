d861266a9389b8050bbd4ee4e213724b7298d017
fanng <xiaojing@datastrato.com>
2025-04-07 18:29:33 +0800

    misc

--- client/java/src/main/java/io/openlineage/client/utils/gravitino/SparkGravitinoInfoProvider.java
+++ client/java/src/main/java/io/openlineage/client/utils/gravitino/SparkGravitinoInfoProvider.java
@@ -1,3 +1,7 @@
+/*
+/* Copyright 2018-2025 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
 package io.openlineage.client.utils.gravitino;
 
 import java.lang.reflect.Method;
